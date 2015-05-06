f61facd6abfd6f25a3079c9143d8c3c98e157e4e
Michael Dao <michael.dao@gmail.com>
2015-05-06 01:12:55 -0600

    Twilio start

--- app/controllers/api/todos_controller.rb
+++ app/controllers/api/todos_controller.rb
@@ -3,6 +3,9 @@ class Api::TodosController < ApplicationController
   def update
     Todo.find(params[:id]).update(active: params[:active])
     render nothing: true
+  end
 
+  def create
+    puts params[:body]
   end
 end
